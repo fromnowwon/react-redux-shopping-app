c8b4bfcf877a3e3d86f96c4252a6a2b1cf0b202d
cw <fromnowwon@gmail.com>
2024-03-12 10:26:45 +0900

    create Home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Navbar />
+			<Home />
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div className="hero">
+			{/* <img src="/assets/bg.png" class="card-img-top" alt="..." /> */}
+			<div class="card border-0">
+				<div className="card-body">
+					<div className="card-img-overlay d-flex flex-column justify-content-center">
+						<div className="container">
+							<h5 class="card-title display-3 fw-folder mb-0">
+								NEW SEASON ARRIVALS
+							</h5>
+							<p class="card-text">CHECK OUT ALL THE TRENDS</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
