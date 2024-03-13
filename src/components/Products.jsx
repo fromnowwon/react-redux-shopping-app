7c521183b475ece88c3b2b446c80625920d846c6
cw <fromnowwon@gmail.com>
2024-03-13 11:17:34 +0900

    feat: set fliter

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -44,17 +44,45 @@ const Products = () => {
 		);
 	};
 
+	const filterProduct = (cat) => {
+		const updatedList = data.filter((x) => x.category === cat);
+		setFilter(updatedList);
+	};
+
 	const ShowProducts = () => {
 		return (
 			<>
 				<div className="buttons d-flex justify-content-center mb-5 pd-5">
-					<button className="btn btn-outline-dark me-2">All</button>
-					<button className="btn btn-outline-dark me-2">Men's Clothing</button>
-					<button className="btn btn-outline-dark me-2">
+					<button
+						className="btn btn-outline-dark me-2"
+						onClick={() => setFilter(data)}
+					>
+						All
+					</button>
+					<button
+						className="btn btn-outline-dark me-2"
+						onClick={() => filterProduct("men's clothing")}
+					>
+						Men's Clothing
+					</button>
+					<button
+						className="btn btn-outline-dark me-2"
+						onClick={() => filterProduct("women's clothing")}
+					>
 						Women's Clothing
 					</button>
-					<button className="btn btn-outline-dark me-2">Jewelery</button>
-					<button className="btn btn-outline-dark me-2">Electronic</button>
+					<button
+						className="btn btn-outline-dark me-2"
+						onClick={() => filterProduct("jewelery")}
+					>
+						Jewelery
+					</button>
+					<button
+						className="btn btn-outline-dark me-2"
+						onClick={() => filterProduct("electronics")}
+					>
+						Electronics
+					</button>
 				</div>
 				{filter.map((product) => {
 					return (
