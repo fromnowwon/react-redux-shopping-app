c6de3ac3c1c78da4cff246599ee45bc308fa970d
cw <fromnowwon@gmail.com>
2024-03-13 11:11:41 +0900

    create Products component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,23 @@
 import React from "react";
+import Products from "./Products";
 
 const Home = () => {
 	return (
 		<div className="hero">
-			{/* <img src="/assets/bg.png" class="card-img-top" alt="..." /> */}
-			<div class="card border-0">
+			{/* <img src="/assets/bg.png" className="card-img-top" alt="..." /> */}
+			<div className="card border-0">
 				<div className="card-body">
 					<div className="card-img-overlay d-flex flex-column justify-content-center">
 						<div className="container">
-							<h5 class="card-title display-3 fw-folder mb-0">
+							<h5 className="card-title display-3 fw-folder mb-0">
 								NEW SEASON ARRIVALS
 							</h5>
-							<p class="card-text">CHECK OUT ALL THE TRENDS</p>
+							<p className="card-text">CHECK OUT ALL THE TRENDS</p>
 						</div>
 					</div>
 				</div>
 			</div>
+			<Products />
 		</div>
 	);
 };
